194d7c2c625bd462c14afa0f65ba369cebb8182e
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-07-31 19:48:35 -0500

    Set up VIF SVG icons

--- src/components/iconComponents/index.ts
+++ src/components/iconComponents/index.ts
@@ -1,1 +1,5 @@
+export { default as LogoInstagram } from "./LogoInstagram";
+export { default as LogoLinkedin } from "./LogoLinkedin";
+export { default as LogoTwitter } from "./LogoTwitter";
+export { default as LogoYoutube } from "./LogoYoutube";
 export { default as VifLogoMark } from "./VifLogoMark";
