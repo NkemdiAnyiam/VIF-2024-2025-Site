c0c58e80396375b25371312981da3d50c6c8042b
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-07-31 23:45:44 -0500

    Implement sticky navbar with background on scroll

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { VifLogoMark } from '../iconComponents';
 
 export function NavBar() {
+  const [sticky, setSticky] = useState(window.scrollY > 0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setSticky(window.scrollY > 0);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  })
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar${sticky ? ' navbar--sticky' : ''}`}>
       <span className="logo-container">
         <VifLogoMark className="vif-logo-mark" />
       </span>
