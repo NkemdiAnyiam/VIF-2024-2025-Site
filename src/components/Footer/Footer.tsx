1bdba29099109af660b4cafab69195a9a69dde55
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-08-01 11:53:19 -0500

    Add hover/active styles to footer icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,19 +7,19 @@ export function Footer(): JSX.Element {
     <footer className="footer">
       <ul className="footer__links">
         <FooterLink href="https://twitter.com/tamuviz?t=GzuSJdHsO5U7NpkGwtHXBA&s=09">
-          <LogoTwitter className="footer__icon" />
+          <LogoTwitter className="footer__icon footer__icon--twitter" />
         </FooterLink>
 
         <FooterLink href="https://youtube.com/c/TexasAMDepartmentofVisualization">
-          <LogoYoutube className="footer__icon" />
+          <LogoYoutube className="footer__icon footer__icon--youtube" />
         </FooterLink>
 
         <FooterLink href="https://instagram.com/tamu_vizindustryfair?utm_medium=copy_link">
-          <LogoInstagram className="footer__icon" />
+          <LogoInstagram className="footer__icon footer__icon--instagram" />
         </FooterLink>
 
         <FooterLink href="https://www.linkedin.com/company/texas-a-m-visualization-industry-fair">
-          <LogoLinkedin className="footer__icon" />
+          <LogoLinkedin className="footer__icon footer__icon--linkedin" />
         </FooterLink>
       </ul>
     </footer>
