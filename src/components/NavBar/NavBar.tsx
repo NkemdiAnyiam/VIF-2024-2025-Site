724d1687ab96f759cd783d20cda2b4c709d17768
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-08-02 14:15:27 -0500

    Set up media query for converting nav links to hamburger menu and implement initial setup for nav overlay

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,49 +5,74 @@ import { VifLogoMark, VifLogoWide } from '../iconComponents';
 
 export function NavBar(): JSX.Element {
   const [sticky, setSticky] = useState(window.scrollY > 0);
+  const [isMobileWidth, setIsMobileWidth] = useState(window.matchMedia(`(max-width: 768px)`).matches);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => {
-      setSticky(window.scrollY > 0);
-    }
-
+    const handleScroll = () => { setSticky(window.scrollY > 0); }
     window.addEventListener('scroll', handleScroll);
+
+    const handleMediaQuery = (e: MediaQueryListEvent) => {
+      setIsMobileWidth(e.matches);
+      setExpanded(false);
+    }
+    const mediaList = window.matchMedia(`(max-width: 768px)`);
+    mediaList.addEventListener('change', handleMediaQuery);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
-    }
-  })
+      mediaList.removeEventListener('change', handleMediaQuery);
+    };
+  });
+
+  const handleHamburgerClick = () => {
+    setExpanded(true);
+  }
 
   return (
-    <nav className={`navbar${sticky ? ' navbar--sticky' : ''}`}>
-      <Link to="/" className="navbar__logo-container">
-          <VifLogoMark className={`vif-logo-mark`} />
+    <nav className={`navbar${sticky ? ' navbar--sticky' : ''}${expanded ? ' navbar--expanded' : ''}`}>
+      <Link to="/" className="navbar__logo-link">
+          <VifLogoMark className={`vif-logo-mark${!sticky ? ' vif-logo--invisible' : ''}`} />
           <VifLogoWide className={`vif-logo-wide${sticky ? ' vif-logo--invisible' : ''}`} />
       </Link>
 
-      <ul className="navbar__links-list">
-        <li className="navbar__item">
-          <Link to="#" className="navbar__link">About<span className="dropdown-marker"></span></Link>
-          
-          <ul className="navbar__nested-list">
-            <Link to="#" className="navbar__link navbar__link--nested">About Viz</Link>
-          </ul>
-        </li>
+      {/* {
+        !isMobileWidth &&
+        <ul className="navbar__links-list">
+          <li className="navbar__item">
+            <Link to="#" className="navbar__link">About<span className="dropdown-marker"></span></Link>
+            
+            <ul className="navbar__nested-list">
+              <Link to="#" className="navbar__link navbar__link--nested">About Viz</Link>
+            </ul>
+          </li>
 
-        <li className="navbar__item">
-          <Link to="#" className="navbar__link">Event Information<span className="dropdown-marker"></span></Link>
+          <li className="navbar__item">
+            <Link to="#" className="navbar__link">Event Information<span className="dropdown-marker"></span></Link>
 
-          <ul className="navbar__nested-list">
-            <Link to="#" className="navbar__link navbar__link--nested">For Students</Link>
-            <Link to="#" className="navbar__link navbar__link--nested">For Industry</Link>
-            <Link to="#" className="navbar__link navbar__link--nested">Common Questions</Link>
-          </ul>
-        </li>
+            <ul className="navbar__nested-list">
+              <Link to="#" className="navbar__link navbar__link--nested">For Students</Link>
+              <Link to="#" className="navbar__link navbar__link--nested">For Industry</Link>
+              <Link to="#" className="navbar__link navbar__link--nested">Common Questions</Link>
+            </ul>
+          </li>
 
-        <li className="navbar__item">
-          <Link to="#" className="navbar__link">Schedule</Link>
-        </li>
-      </ul>
+          <li className="navbar__item">
+            <Link to="#" className="navbar__link">Schedule</Link>
+          </li>
+        </ul>
+      } */}
+
+      {
+        // isMobileWidth &&
+        <div onClick={handleHamburgerClick} className="navbar__hamburger">
+          <button className="navbar__hamburger-button">
+            <div className="navbar__hamburger-bar"></div>
+            <div className="navbar__hamburger-bar"></div>
+            <div className="navbar__hamburger-bar"></div>
+          </button>
+        </div>
+      }
     </nav>
   );
 }
