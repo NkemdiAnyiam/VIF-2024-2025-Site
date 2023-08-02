2b84226c595f57732dfd9eabc9e2723ff7d8ddea
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-08-02 15:58:20 -0500

    Implement modal nav

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { VifLogoMark, VifLogoWide } from '../iconComponents';
+import { VifLogoMark, VifLogoWide, XSign } from '../iconComponents';
 
+// TODO: implement focus trap when in modal
 export function NavBar(): JSX.Element {
   const [sticky, setSticky] = useState(window.scrollY > 0);
   const [isMobileWidth, setIsMobileWidth] = useState(window.matchMedia(`(max-width: 768px)`).matches);
@@ -15,6 +16,7 @@ export function NavBar(): JSX.Element {
     const handleMediaQuery = (e: MediaQueryListEvent) => {
       setIsMobileWidth(e.matches);
       setExpanded(false);
+      document.querySelector('body')?.classList.remove('scroll-disabled');
     }
     const mediaList = window.matchMedia(`(max-width: 768px)`);
     mediaList.addEventListener('change', handleMediaQuery);
@@ -27,30 +29,36 @@ export function NavBar(): JSX.Element {
 
   const handleHamburgerClick = () => {
     setExpanded(true);
+    document.querySelector('body')?.classList.add('scroll-disabled');
+  }
+
+  const handleXClick = () => {
+    setExpanded(false);
+    document.querySelector('body')?.classList.remove('scroll-disabled');
   }
 
   return (
     <nav className={`navbar${sticky ? ' navbar--sticky' : ''}${expanded ? ' navbar--expanded' : ''}`}>
       <Link to="/" className="navbar__logo-link">
-          <VifLogoMark className={`vif-logo-mark${!sticky ? ' vif-logo--invisible' : ''}`} />
-          <VifLogoWide className={`vif-logo-wide${sticky ? ' vif-logo--invisible' : ''}`} />
+          <VifLogoMark className={`vif-logo-mark${(sticky || expanded) ? '' : ' vif-logo--invisible'}`} />
+          <VifLogoWide className={`vif-logo-wide${!(sticky || expanded) ? '' : ' vif-logo--invisible'}`} />
       </Link>
 
-      {/* {
-        !isMobileWidth &&
-        <ul className="navbar__links-list">
+      {
+        (!isMobileWidth || expanded) &&
+        <ul className={`navbar__links-list${expanded ? ' navbar__links-list--expanded' : ''}`}>
           <li className="navbar__item">
-            <Link to="#" className="navbar__link">About<span className="dropdown-marker"></span></Link>
+            <span tabIndex={0} className="navbar__link navbar__link--top-level">About<span className="dropdown-marker"></span></span>
             
-            <ul className="navbar__nested-list">
+            <ul className={`navbar__nested-list${expanded ? ' navbar__nested-list--expanded' : ''}`}>
               <Link to="#" className="navbar__link navbar__link--nested">About Viz</Link>
             </ul>
           </li>
 
           <li className="navbar__item">
-            <Link to="#" className="navbar__link">Event Information<span className="dropdown-marker"></span></Link>
+            <span tabIndex={0} className="navbar__link navbar__link--top-level">Event Information<span className="dropdown-marker"></span></span>
 
-            <ul className="navbar__nested-list">
+            <ul className={`navbar__nested-list${expanded ? ' navbar__nested-list--expanded' : ''}`}>
               <Link to="#" className="navbar__link navbar__link--nested">For Students</Link>
               <Link to="#" className="navbar__link navbar__link--nested">For Industry</Link>
               <Link to="#" className="navbar__link navbar__link--nested">Common Questions</Link>
@@ -61,17 +69,25 @@ export function NavBar(): JSX.Element {
             <Link to="#" className="navbar__link">Schedule</Link>
           </li>
         </ul>
-      } */}
+      }
 
       {
-        // isMobileWidth &&
-        <div onClick={handleHamburgerClick} className="navbar__hamburger">
-          <button className="navbar__hamburger-button">
-            <div className="navbar__hamburger-bar"></div>
-            <div className="navbar__hamburger-bar"></div>
-            <div className="navbar__hamburger-bar"></div>
-          </button>
-        </div>
+        isMobileWidth &&
+        (
+          expanded ?
+
+          <button onClick={handleXClick} className="navbar__close-button">
+            <XSign className="navbar__close-button-icon" />
+          </button> :
+
+          <div onClick={handleHamburgerClick} className="navbar__hamburger">
+            <button className="navbar__hamburger-button">
+              <div className="navbar__hamburger-bar"></div>
+              <div className="navbar__hamburger-bar"></div>
+              <div className="navbar__hamburger-bar"></div>
+            </button>
+          </div>
+        )
       }
     </nav>
   );
--- src/components/iconComponents/index.ts
+++ src/components/iconComponents/index.ts
@@ -4,3 +4,4 @@ export { default as LogoTwitter } from "./LogoTwitter";
 export { default as LogoYoutube } from "./LogoYoutube";
 export { default as VifLogoMark } from "./VifLogoMark";
 export { default as VifLogoWide } from "./VifLogoWide";
+export { default as XSign } from "./XSign";
