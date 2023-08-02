94d7461532a22e9366eaa66ae74c2ddab9b1b1dd
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-08-02 01:14:06 -0500

    Switch logo mark for wide logo  depending on sticky status and start setting up nav links

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { VifLogoMark, VifLogoWide } from '../iconComponents';
 
@@ -19,11 +20,25 @@ export function NavBar(): JSX.Element {
 
   return (
     <nav className={`navbar${sticky ? ' navbar--sticky' : ''}`}>
-      <span className="logo-container">
-        <VifLogoMark className="vif-logo-mark" />
-        {/* <VifLogoWide className="vif-logo-wide" /> */}
-      </span>
-      Nav
+      <Link to="/" className="logo-container">
+        {
+          sticky ?
+          <VifLogoMark className="vif-logo-mark" /> :
+          <VifLogoWide className="vif-logo-wide" />
+        }
+      </Link>
+
+      <ul className="navbar__links-list">
+        <li className="navbar__item">
+          <Link to="#" className="navbar__link">About</Link>
+        </li>
+        <li className="navbar__item">
+          <Link to="#" className="navbar__link">Event Information</Link>
+        </li>
+        <li className="navbar__item">
+          <Link to="#" className="navbar__link">Schedule</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
