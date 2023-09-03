fa055ec6df0f76482e45eb767c79146368bb8970
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-09-03 00:52:28 -0500

    Add webp images for program images

--- src/components/PictureSquare/PictureSquare.tsx
+++ src/components/PictureSquare/PictureSquare.tsx
@@ -12,11 +12,11 @@ export function PictureSquare(props: PictureSquareProps): JSX.Element {
     <div className={`picture-square${props.color ? ` picture-square--${props.color}` : ''}`}>
       <p className="picture-square__text">{props.text}</p>
       <div className="picture-square__background">
-        <img
-          src={require(`../../images/programs/${props.imageName}.jpg`)}
-          alt={props.altText}
-          className="picture-square__image"
-        />
+        <picture className="picture-square__picture">
+          <source srcSet={require(`../../images/programs/${props.imageName}.webp`)} type="image/webp" />
+          <source srcSet={require(`../../images/programs/${props.imageName}.jpg`)} type="image/jpeg" />
+          <img src={require(`../../images/programs/${props.imageName}.jpg`)} alt={props.altText} className="picture-square__image" />
+        </picture>
       </div>
     </div>
   );
