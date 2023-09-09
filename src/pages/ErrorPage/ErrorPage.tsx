970a1a52df1b26d723dfb4d8601749e49643d6e9
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-09-08 22:46:31 -0500

    Add link around logo on 404 page directing back to homepage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { SearchEngineOpt } from '../../components/SearchEngineOpt/SearchEngineOpt';
 
@@ -18,7 +19,7 @@ export function ErrorPage(props: ErrorPageProps): JSX.Element {
       <section className="section section--error">
         <div className="container">
           <div className="error">
-            <VifLogoMark className="error__logo-mark" />
+            <Link to="/"><VifLogoMark className="error__logo-mark" /></Link>
             <p className="error__message">{props.message}</p>
           </div>
         </div>
