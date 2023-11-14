7aeeac735bddbe4834831af5d83286ce1cff34d0
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-11-14 17:57:57 -0600

    Set up function for rendering row cells and set up initial style for header row

--- src/components/Timetable/Timetable.tsx
+++ src/components/Timetable/Timetable.tsx
@@ -5,9 +5,28 @@ import { TimetableRow } from './TimetableRow/TimetableRow';
 export function Timetable(): JSX.Element {
   return (
     <div className={`timetable`}>
-      <TimetableRow />
-      <TimetableRow />
-      <TimetableRow />
+      <TimetableRow
+        isHeader
+        cellData={[
+          'Companies',
+          '10:00',
+          '10:30',
+          '11:00',
+          '11:30',
+          '12:00',
+          '12:30',
+          '1:00',
+          '1:30',
+          '2:00',
+          '2:30',
+        ]}
+      />
+      {/* <TimetableRow
+        cellData={[]}
+      />
+      <TimetableRow
+        cellData={[]}
+      /> */}
     </div>
   );
 }
--- src/components/Timetable/TimetableRow/TimetableRow.tsx
+++ src/components/Timetable/TimetableRow/TimetableRow.tsx
@@ -2,19 +2,26 @@ import React from 'react';
 
 interface TimetableRowProps {
   isHeader?: boolean;
+  cellData: string[];
 }
 
-export function TimetableRow({isHeader}: TimetableRowProps): JSX.Element {
+const renderCells = (cellData: string[]): JSX.Element[] => {
+  const row = cellData.map((str) => {
+    return (
+      <div className="cell"><span>{str}</span></div>
+    );
+  });
+
+  row.splice(1, 0, <div className="cell cell--spacer"></div>);
+  row.push(<div className="cell cell--spacer"></div>);
+
+  return row;
+}
+
+export function TimetableRow({isHeader, cellData}: TimetableRowProps): JSX.Element {
   return (
     <div className={`timetable__row${isHeader ? ' timetable__row--header' : ''}`}>
-      <div className="cell cell--1">A</div>
-      <div className="cell">B</div>
-      <div className="cell">C</div>
-      <div className="cell">D</div>
-      <div className="cell">E</div>
-      <div className="cell">E</div>
-      <div className="cell">E</div>
-      <div className="cell">E</div>
+      {renderCells(cellData)}
     </div>
   );
 }
