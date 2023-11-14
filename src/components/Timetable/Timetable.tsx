e66e6a854867a9de6916431f08810a6098b1fdef
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-11-14 17:17:56 -0600

    Set up initial structure for rows, headers, and overflow behavior

--- src/components/Timetable/Timetable.tsx
+++ src/components/Timetable/Timetable.tsx
@@ -4,6 +4,10 @@ import { TimetableRow } from './TimetableRow/TimetableRow';
 
 export function Timetable(): JSX.Element {
   return (
-    <div className={`timetable`}>Timetable</div>
+    <div className={`timetable`}>
+      <TimetableRow />
+      <TimetableRow />
+      <TimetableRow />
+    </div>
   );
 }
--- src/components/Timetable/TimetableRow/TimetableRow.tsx
+++ src/components/Timetable/TimetableRow/TimetableRow.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-export function TimetableRow(): JSX.Element {
+interface TimetableRowProps {
+  isHeader?: boolean;
+}
+
+export function TimetableRow({isHeader}: TimetableRowProps): JSX.Element {
   return (
-    <div className={`timetable__row`}>Timetable Row</div>
+    <div className={`timetable__row${isHeader ? ' timetable__row--header' : ''}`}>
+      <div className="cell cell--1">A</div>
+      <div className="cell">B</div>
+      <div className="cell">C</div>
+      <div className="cell">D</div>
+      <div className="cell">E</div>
+      <div className="cell">E</div>
+      <div className="cell">E</div>
+      <div className="cell">E</div>
+    </div>
   );
 }
