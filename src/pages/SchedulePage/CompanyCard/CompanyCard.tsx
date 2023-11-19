2c628d9f86812b22d3361bf8700e3858dc6f01d6
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2023-11-19 12:15:46 -0600

    Begin setting up section for displaying company cards

---
+++ src/pages/SchedulePage/CompanyCard/CompanyCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface CompanyCardProps {
+
+}
+
+export function CompanyCard(props: CompanyCardProps): JSX.Element {
+  return (
+    <div className="company-card">
+      Card
+    </div>
+  );
+}
