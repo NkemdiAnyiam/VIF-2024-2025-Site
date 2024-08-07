cc1830040d1ea9bc9095d6032383cf7ac4d3296d
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2024-08-06 19:32:43 -0500

    Update VIF Committee page for 2024–25

--- src/pages/About/VifCommitteePage/VifCommitteePage.tsx
+++ src/pages/About/VifCommitteePage/VifCommitteePage.tsx
@@ -7,27 +7,14 @@ import { Header } from '../../../components/Header/Header';
 
 const committeeMembers: CommitteeBlockProps[] = [
   {
-    fullName: `Kaylyn Luna`,
-    role: `Committee Co-Director`,
-    rank: 1,
-    imageName: `Kaylyn-Luna-min`,
-    socials: {
-      linkedin: `https://www.linkedin.com/in/kaylyn-luna-5378521a7`,
-      website: `https://artbykaylynluna.godaddysites.com/`,
-    },
-    description: `Kaylyn is a graduate student pursuing her Masters of Science in Visualization with a focus on digital environments and layout for games. She is currently working as lead concept artist for the LIVE LAB where she creates illustrations, animations, video graphics and other digital content for the University. Outside of class, Kaylyn enjoys digital painting, cooking and reading comic books.`,
-  },
-
-  {
     fullName: `Jenny Tran`,
     role: `Committee Co-Director`,
     rank: 1,
     imageName: `Jenny-Tran-min`,
     socials: {
       linkedin: `https://www.linkedin.com/in/jennyhantran/`,
-      website: `https://www.jennyhtran.com/`
     },
-    description: `Jenny Tran is a Junior currently working towards her B.S. in Visualization as well as a minor in Film Studies. Her focus is on project management/3D modeling and she is passionate about pursuing a future career in the animated film industry. She is currently serving as President of TAMU Women in Animation. She also works as a 3D Artist for the SOFT Interaction Lab. Outside of class, Jenny enjoys playing video games and taking care of her pet snail.`,
+    description: `Jenny Tran is a Senior currently working towards her B.S. in Visualization as well as a minor in Film Studies. Her focus is on project management, and she is passionate about pursuing a future career in the animated film industry. She is currently serving as President of TAMU Women in Animation. Outside of class, Jenny enjoys playing video games and hanging out with her cats.`,
   },
 
   {
@@ -41,19 +28,7 @@ const committeeMembers: CommitteeBlockProps[] = [
       linkedin: `https://www.linkedin.com/in/nkemdi-anyiam/`,
       github: `https://github.com/NkemdiAnyiam`,
     },
-    description: `Nkemdi Anyiam is a graduate student pursuing a Master of Science in Computer Science at Texas A&M University. He is currently working as a graduate research assistant, focusing primarily on web visualizations and computer science education.`,
-  },
-
-  {
-    fullName: `Nimi Parmar`,
-    role: `TV/Marketing Lead`,
-    rank: 2,
-    imageName: `Nimi-Parmar-min`,
-    socials: {
-      linkedin: `https://www.linkedin.com/in/nimi-parmar`,
-      website: `https://nimiparmar06.wixsite.com/modeler`,
-    },
-    description: `Nimi is a student of PVFA pursuing Master of Science in Visualization. She would like to be a part of the animation industry as a 3D modeler and look dev artist. She comes from a unique undergrad background with a B.S. in biological sciences but now enjoys UV unwrapping in her free time. For the past year, she has served as a graduate assistant for classes like art history and game design.`,
+    description: `Nkemdi Anyiam is a graduate student pursuing a PhD in Computer Science at Texas A&M University. He also graduated with his Master’s and Bachelor’s in Computer Science and double-minored in Art (New Media) and Game Design & Development at A&M as well. He is currently working as a graduate research assistant, focusing primarily on web visualizations and computer science education. His hobbies include Latin dance, rock climbing, gaming, watching anime, and voice-acting.`,
   },
 
   {
@@ -61,37 +36,15 @@ const committeeMembers: CommitteeBlockProps[] = [
     role: `Faculty Lead Liaison`,
     rank: 3,
     imageName: `Mayet-Andreassen-min`,
-    socials: {},
-    description: `Mayet Andreassen is currently working as a full-time Instructional Assistant Professor and Associate Program Director in the Visualization Department at Texas A&M University. She is also a member of the TXMPA (Texas Motion Picture Assoc.) Board as the Education Representative. She has over 10 years of teaching experience with a focus in games and animation. She previously worked as a game and front-end artist, as well as a freelance illustrator and graphics artist, prior to becoming an educator.`,
-  },
-
-  {
-    fullName: `Andersen Wood`,
-    role: `Event Management Lead`,
-    rank: 2,
-    imageName: `Andersen-Wood-min`,
     socials: {
-      linkedin: `https://www.linkedin.com/in/andersenwood/`,
-      website: `https://www.andersenwood.com/`,
+      linkedin: `https://www.linkedin.com/in/mayet-andreassen/`,
     },
-    description: `Andersen is a Junior Visualization student pursuing his B.S. degree. His focus is in Project Management and Character Rigging. In addition, he is the Director of Chillennium, Director of GigaJam, Interstitials Director of Viz-A-GoGo, Vice president of A&M's ACM SIGGRAPH Chapter, and Lead Student Technician for the Visualization Program.`,
-  },
-
-  {
-    fullName: `Emilie Sangerhausen`,
-    role: `Outreach Lead`,
-    rank: 2,
-    imageName: `Emilie-Sangerhausen-min`,
-    socials: {
-      linkedin: `https://www.linkedin.com/in/emilie-sangerhausen-35695a1a2/ `,
-      website: `https://esangerhausen.wixsite.com/portfolio`,
-    },
-    description: `Emilie is a senior Visualization student currently working towards her B.S. degree. After college, her dream is to be a visual development/character artist in the animation and games industry. Outside of VIF, she serves as the Student Senator for SPVFA, the Visualization Student Technician Graphic Design Lead, Outreach Lead for Viz-A-GoGo, and is a musician in the TAMU Wind Symphony. When she is not creating art or performing, she enjoys running and taking care of plants.`,
+    description: `Mayet Andreassen is currently working as a full-time Instructional Assistant Professor and Internship Coordinator in the Visualization Department at Texas A&M University. She is also a member of the TXMPA (Texas Motion Picture Assoc.) Board as the Education Representative. She has over 10 years of teaching experience with a focus in games and animation. She previously worked as a game and front-end artist, as well as a freelance illustrator and graphics artist, prior to becoming an educator.`,
   },
 
   {
     fullName: `Jocylin "Joy" Lopez`,
-    role: `Video Editor`,
+    role: `TV/Marketing Lead`,
     rank: 2,
     imageName: `Jocylin-Lopez-min`,
     socials: {
@@ -100,6 +53,41 @@ const committeeMembers: CommitteeBlockProps[] = [
     },
     description: `Joy is a graduate student pursuing a Master's in Visualization, specializing in character design, modeling, and game design. She works in marketing at the LIVE LAB, manages social media for the university, and holds roles as Social Media Officer for 2D Visual Developers and Lead Concept Artist mentor. Outside of her academic and professional life, Joy enjoys video games, anime, and oil painting.`,
   },
+
+  {
+    fullName: `Skylar La`,
+    role: `Committee Co-Director`,
+    rank: 1,
+    imageName: `Skylar-La-min`,
+    socials: {
+      linkedin: `https://www.linkedin.com/in/trang-thi-thuy-la-556a712b4/`,
+    },
+    description: `Skylar La is a second-year graduate student pursuing a Master of Science in Visualization at Texas A&M University. She owns 2 university degrees (one in Economics, the other in Multimedia). However, she chose to pursue 3D for her career path, especially focusing on Modelling and Layout. She is now the secretary of TAMU VGSA (Texas A&M Visualization Graduate Student Association) and serves as a graduate assistant for Visual studio classes. Her hobbies include playing instruments and composing music.`,
+  },
+
+  {
+    fullName: `Shea Stafford`,
+    role: `Event Management Lead`,
+    rank: 2,
+    imageName: `Shea-Stafford-min`,
+    socials: {
+      linkedin: `www.linkedin.com/in/sheastafford`,
+      website: `https://sheastafford.myportfolio.com/`,
+    },
+    description: `Shea Stafford is a Sophomore Visualization student pursuing her B.S. degree. Her focus is in Project Management, Surfacing, and Lighting. In addition, she is the Logistics Coordinator of GigaJam, a member of the HIVE, and Industry Relations for A&M’s ACM SIGGRAPH Chapter. She also enjoys learning about animation history, reading fantasy novels, watching movies, and drawing.`,
+  },
+
+  {
+    fullName: `Marisa "MJ" Jacques`,
+    role: `Outreach Lead`,
+    rank: 2,
+    imageName: `Marisa-Jacques-min`,
+    socials: {
+      linkedin: `https://www.linkedin.com/in/marisa-jacques-74ab16185/`,
+      website: `https://marisajacquescolle.wixsite.com/marisa-jacques`,
+    },
+    description: `MJ is a first-year graduate student pursuing a Master of Science in Visualization, and she earned her B.S. in Animation from Sam Houston State University in 2022. She is currently focusing on developing her skills in 3D surfacing, and she has had an ongoing interest in filmmaking since childhood. MJ works as a graduate assistant for Research in Visualization Tech and enjoys talking to students about their capstone projects to help them develop strong ideas. On the weekends, she works on side projects and spends time in the company of her many animals.`,
+  }
 ];
 
 const renderVifCommBlocks = (items: CommitteeBlockProps[]) => {
@@ -125,7 +113,7 @@ export function VifCommitteePage(): JSX.Element {
   return (
     <main className="vif-committee-page">
       <SearchEngineOpt
-        title="VIF Committee | Viz Industry Fair 2023–24"
+        title="VIF Committee | Viz Industry Fair 2024–25"
         description="Introducing the VIF committee, our dedicated students and faculty who help make the Visualization Industry Fair possible!"
       />
 
